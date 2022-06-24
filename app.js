45c17217448dfc043fa76bae11eb796538c4a084
gokhan <kemasgokhan@gmail.com>
2022-06-24 10:55:45 +0300

    j

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const getAllCountry = async() =>{
 //     item.addEventListener('mouseup', (e) =>{
 //       e.preventDefault();   
 //       cname = e.target.innerText;
-//     }) uy
+//     })
 //   }
 // )}
 
